175713391c14a1876118d13583f5da10622bf0cb
Fajarsya <f4j4rkokok@gmail.com>
2023-02-13 15:06:37 +0700

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUrlFile = New System.Windows.Forms.TextBox()
+        Me.btnOpenFile = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnKata = New System.Windows.Forms.Button()
+        Me.btnBaris = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.dgview = New System.Windows.Forms.DataGridView()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.dgview, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("a Atomic Md", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(44, 120)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(67, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Pilih file"
+        '
+        'txtUrlFile
+        '
+        Me.txtUrlFile.Location = New System.Drawing.Point(109, 116)
+        Me.txtUrlFile.Name = "txtUrlFile"
+        Me.txtUrlFile.Size = New System.Drawing.Size(237, 20)
+        Me.txtUrlFile.TabIndex = 1
+        '
+        'btnOpenFile
+        '
+        Me.btnOpenFile.Location = New System.Drawing.Point(352, 117)
+        Me.btnOpenFile.Name = "btnOpenFile"
+        Me.btnOpenFile.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpenFile.TabIndex = 2
+        Me.btnOpenFile.Text = "browse file"
+        Me.btnOpenFile.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnKata
+        '
+        Me.btnKata.Location = New System.Drawing.Point(109, 169)
+        Me.btnKata.Name = "btnKata"
+        Me.btnKata.Size = New System.Drawing.Size(75, 23)
+        Me.btnKata.TabIndex = 3
+        Me.btnKata.Text = "Jumlah kata"
+        Me.btnKata.UseVisualStyleBackColor = True
+        '
+        'btnBaris
+        '
+        Me.btnBaris.Location = New System.Drawing.Point(190, 169)
+        Me.btnBaris.Name = "btnBaris"
+        Me.btnBaris.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaris.TabIndex = 4
+        Me.btnBaris.Text = "Jumlah baris"
+        Me.btnBaris.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(271, 169)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "bersihkan"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(352, 169)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 6
+        Me.btnClose.Text = "keluar"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'dgview
+        '
+        Me.dgview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgview.Location = New System.Drawing.Point(47, 207)
+        Me.dgview.Name = "dgview"
+        Me.dgview.Size = New System.Drawing.Size(380, 97)
+        Me.dgview.TabIndex = 7
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(0, -1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(485, 79)
+        Me.Panel1.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Palatino Linotype", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(75, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(332, 26)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Aplikasi Penghitung Kata dan Baris"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(483, 378)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.dgview)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnBaris)
+        Me.Controls.Add(Me.btnKata)
+        Me.Controls.Add(Me.btnOpenFile)
+        Me.Controls.Add(Me.txtUrlFile)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgview, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUrlFile As TextBox
+    Friend WithEvents btnOpenFile As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnKata As Button
+    Friend WithEvents btnBaris As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnClose As Button
+    Friend WithEvents dgview As DataGridView
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub btnOpenFile_Click(sender As Object, e As EventArgs) Handles btnOpenFile.Click
+        OpenFileDialog1.Filter = "Excel,Word|*.xls;*.xlsx;*.Doc;*.Docx"
+        OpenFileDialog1.ShowDialog()
+        txtUrlFile.Text = OpenFileDialog1.FileName
+    End Sub
+
+    Private Sub btnKata_Click(sender As Object, e As EventArgs) Handles btnKata.Click
+        If String.IsNullOrWhiteSpace(txtUrlFile.Text) Then
+            MessageBox.Show("pilih file terlebih dahulu !", "Anda belum memilih file", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            hitungKata(txtUrlFile.Text)
+        End If
+    End Sub
+
+    Private Sub btnBaris_Click(sender As Object, e As EventArgs) Handles btnBaris.Click
+        If String.IsNullOrWhiteSpace(txtUrlFile.Text) Then
+            MessageBox.Show("pilih file terlebih dahulu !", "Anda belum memilih file", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            Dim namaFile As String = txtUrlFile.Text
+            If File.Exists(namaFile) Then
+                Dim baris() As String = File.ReadAllLines(namaFile)
+                dataView("jumlah baris", baris.Length)
+            Else
+                MessageBox.Show("file tidak di temukan", "error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        End If
+    End Sub
+
+    Function hitungKata(ByVal namaFile As String)
+        Dim jumlahKata As Integer = 0
+        If File.Exists(namaFile) Then
+            Using sr As New StreamReader(namaFile)
+                Dim baris As String
+                Do
+                    baris = sr.ReadLine()
+                    If baris IsNot Nothing Then
+                        jumlahKata += baris.Split(" "c).Length
+                    End If
+                Loop Until baris Is Nothing
+                sr.Close()
+            End Using
+            dataView("Jumlah kata", jumlahKata)
+        Else
+            MessageBox.Show("file tidak di temukan", "error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        Return jumlahKata
+    End Function
+
+    Function dataView(ByVal keterangan As String, ByVal jumlah As Integer)
+        Dim dt As New DataTable()
+        dt.Columns.Add("Keterangan")
+        dt.Columns.Add("jumlah")
+
+        dt.Rows.Add(keterangan, jumlah)
+
+        dgview.DataSource = dt
+        dgview.Columns(0).Width = 187
+        dgview.Columns(1).Width = 150
+
+    End Function
+
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtUrlFile.Clear()
+        dgview.DataSource = Nothing
+        dgview.Rows.Clear()
+        dgview.Columns.Clear()
+    End Sub
+
+End Class
